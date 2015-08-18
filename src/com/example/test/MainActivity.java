288306845f03add1e7d053c1522a760dd4655517
jsjqcs <835270736@qq.com>
2015-08-18 09:32:08 +0800

    测试

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,5 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         System.out.println("hello world");
+        Log.d("MainActivity", "这里只是做一个git建立网络代码仓库测试");
     }
 }
